641009f45c8da9eb4b851ebf12ec27a085e9490e
Yana Pavelchuk <yapavelchuk@yandex.ru>
2023-04-03 20:19:33 +0500

    create index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
+import genDiff from '../src/index.js';
+import {Command} from 'commander';
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('')
-  // .option('-V, --version', 'output the version number')
-  // .option('-h, --help', 'display help for command');
-  
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
+
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import process from 'process';
+import path from 'path';
+import _ from 'lodash';
+
+const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
+const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
+// const getExtension = (file) => path.extname(file);
+
+const genDiff = (filepath1, filepath2) => {
+  const file1data = JSON.parse(readFile(getAbsolutePath(filepath1))); 
+  const file2data = JSON.parse(readFile(getAbsolutePath(filepath2)));
+  const file1keys = Object.keys(file1data);
+  const file2keys = Object.keys(file2data);
+  const allKeys = _.union(file1keys, file2keys).sort();
+
+  const diff = allKeys.map((key) => {
+    if (!_.has(file2data, key)) {
+      return `- ${key}: ${file1data[key]}`;
+    }
+    if (!_.has(file1data, key)) {
+      return `+ ${key}: ${file2data[key]}`;
+    }
+    if (_.isEqual(file1data[key],file2data[key])) {
+      return `  ${key}: ${file2data[key]}`;
+    }
+    return ` - ${key}: ${file1data[key]}\n  + ${key}: ${file2data[key]}`;
+  })
+  return `{\n${diff.join('\n')}\n}`;
+};
+
+export default genDiff;
