168a7638e6d10ff8e501220536e1e6cae91a9056
Yana Pavelchuk <yapavelchuk@yandex.ru>
2023-05-30 15:44:36 +0500

    check

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,47 +1,53 @@
-import url from 'url';
-import fs from 'fs';
-import path, { dirname } from 'node:path';
+// // import url from 'url';
+// import fs from 'fs';
 // import path from 'path';
-import genDiff from '../src/index.js';
+// import genDiff from '../src/index.js';
 
-// Определяем путь к директории с фикстурами
-const fixturesPath = path.join(path.dirname(new URL(import.meta.url).pathname), '../__fixtures__');
-const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
-const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
+// // Определяем путь к директории с фикстурами
+// const fixturesPath = path.join(path.dirname(new URL(import.meta.url).pathname), '../__fixtures__');
+// const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
+// const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
-// Test case 1: Compare two empty JSON files
-// test('Compare two empty JSON files', () => {
+// // test('Compare JSON files with identical contents', () => {
+// //   const filepath1 = path.join(fixturesPath, 'file1.json');
+// //   const filepath2 = path.join(fixturesPath, 'file1.json');
+// //   const filepathExpected = path.join(fixturesPath, 'stylishResult.txt');
+// //   const expected = JSON.parse(readFile(getAbsolutePath(filepathExpected)));
+// //   // {
+// //   //   "name": "Yana",
+// //   //   "status": true
+// //   // };
+// //   const result = genDiff(filepath1, filepath2);
+// //   expect(result).toEqual(expected);
+// // });
+
+// // Test case 3: Compare JSON files with different contents
+// test('Compare JSON files with different contents', () => {
 //   const filepath1 = path.join(fixturesPath, 'file1.json');
-//   const filepath2 = path.join(fixturesPath, 'file2.json');
-//   const expected = '{\n}\n';
+//   const filepath2 = path.join(fixturesPath, 'file3.json');
+//   const filepathExpected = path.join(fixturesPath, 'stylishResult.txt');
+//   const expected = JSON.parse(readFile(getAbsolutePath(filepathExpected)));
 //   const result = genDiff(filepath1, filepath2);
 //   expect(result).toEqual(expected);
 // });
+import fs from 'fs';
+import path from 'path';
+import * as jest from 'jest';
+import genDiff from '../src/index.js';
 
-// Test case 2: Compare JSON files with identical contents
-test('Compare JSON files with identical contents', () => {
-  const filepath1 = path.join(fixturesPath, 'file1.json');
-  const filepath2 = path.join(fixturesPath, 'file1.json');
-  const filepathExpected = path.join(fixturesPath, 'fileexpected.json');
-  const expected = JSON.parse(readFile(getAbsolutePath(filepathExpected)));
-  // {
-  //   "name": "Yana",
-  //   "status": true
-  // };
-  const result = genDiff(filepath1, filepath2);
-  expect(result).toEqual(expected);
-});
+const { expect } = jest.default;
 
-// Test case 3: Compare JSON files with different contents
-test('Compare JSON files with different contents', () => {
+// const fixturesPath = path.join(__dirname, '../__fixtures__');
+const fixturesPath = path.join(path.dirname(new URL(import.meta.url).pathname), '../__fixtures__');
+
+const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
+const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
+
+it('should compare JSON files with different contents', () => {
   const filepath1 = path.join(fixturesPath, 'file1.json');
   const filepath2 = path.join(fixturesPath, 'file3.json');
-  const expected = {
-    age: 24,
-    name: 'Yana',
-    status: true
-    
-  };
+  const filepathExpected = path.join(fixturesPath, 'stylishResult.txt');
+  const expected = JSON.parse(readFile(getAbsolutePath(filepathExpected)));
   const result = genDiff(filepath1, filepath2);
   expect(result).toEqual(expected);
 });
