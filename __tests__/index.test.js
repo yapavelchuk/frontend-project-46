81eb681b3c7019464a49c21818e879a0db99b682
Yana Pavelchuk <yapavelchuk@yandex.ru>
2023-06-02 20:57:16 +0500

    add test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,13 @@
 import fs from 'fs';
 import path from 'path';
-import * as jest from 'jest';
 import genDiff from '../src/index.js';
+import { expect } from 'expect';
 
-const { expect } = jest.default;
-
-const fixturesPath = path.join(__dirname, '../__fixtures__');
+const fixturesPath = path.join(
+  path.dirname(new URL(import.meta.url).pathname),
+  '..',
+  '__fixtures__'
+);
 
 const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
 const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
@@ -14,7 +16,8 @@ it('should compare JSON files with different contents', () => {
   const filepath1 = path.join(fixturesPath, 'file1.json');
   const filepath2 = path.join(fixturesPath, 'file3.json');
   const filepathExpected = path.join(fixturesPath, 'stylishResult.txt');
-  const expected = JSON.parse(readFile(getAbsolutePath(filepathExpected)));
+  const expectedString = readFile(getAbsolutePath(filepathExpected));
+  const expected = expectedString.trim();
   const result = genDiff(filepath1, filepath2);
   expect(result).toEqual(expected);
 });
