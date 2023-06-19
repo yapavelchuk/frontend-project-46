bdcde6825417ecbee097c174268d51a4cf543d3d
Yana Pavelchuk <yapavelchuk@yandex.ru>
2023-06-19 21:11:27 +0600

    update index.js file

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 #!/usr/bin/env node
 
-import _ from 'lodash';
 import parseData from './parser.js';
+import buildTree from './buildTree.js';
+import formatter from './formatters/index.js';
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const file1data = parseData(filepath1);
   const file2data = parseData(filepath2);
-  const file1keys = Object.keys(file1data);
-  const file2keys = Object.keys(file2data);
-  const allKeys = _.union(file1keys, file2keys).sort();
-
-  const diff = allKeys.map((key) => {
-    if (!_.has(file2data, key)) {
-      return `- ${key}: ${file1data[key]}`;
-    }
-    if (!_.has(file1data, key)) {
-      return `+ ${key}: ${file2data[key]}`;
-    }
-    if (_.isEqual(file1data[key], file2data[key])) {
-      return `  ${key}: ${file2data[key]}`;
-    }
-    return ` - ${key}: ${file1data[key]}\n  + ${key}: ${file2data[key]}`;
-  });
-  return `{\n${diff.join('\n')}\n}`;
+  const tree = buildTree(file1data, file2data);
+  return formatter(tree, format);
 };
 
 export default genDiff;
