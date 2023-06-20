dd801fc8270757a616acf42ca3d8af74bcd78e05
Yana Pavelchuk <yapavelchuk@yandex.ru>
2023-06-20 18:43:41 +0600

    update formatters

---
+++ src/formatters/plain.js
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+
+const stringify = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  // if (_.isString(value)) {
+  if (typeof value === 'string') {
+    return `${value}`;
+  }
+  return value;
+};
+
+const plain = (ast) => {
+  const iter = (tree, path) => {
+    const result = tree.flatMap((node) => {
+      switch (node.type) {
+        case 'nested':
+          return iter(node.children, `${path}${node.name}.`);
+        case 'added':
+          return `Property '${path}${node.name}' was added with value: ${stringify(node.value)}`;
+        case 'removed':
+          return `Property '${path}${node.name}' was removed`;
+        case 'unchanged':
+          return [];
+        case 'changed':
+          return `Property '${path}${node.name}' was updated. From ${stringify(node.valueBefore)} to ${stringify(node.valueAfter)}`;
+        default:
+          throw new Error(`Unexpected node type: ${node.type}`);
+      }
+    });
+    return result.join('\n');
+  };
+  return iter(ast, '');
+};
+
+export default plain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,39 +1,85 @@
+// import _ from 'lodash';
+
+// const generateSpaces = (depth) => ' '.repeat(depth * 4 - 2);
+
+// const stringify = (node, depth) => {
+//   if (_.isObject(node)) {
+//     const entries = Object.entries(node);
+//     const result = entries.map(([key, value]) => `${generateSpaces(depth + 1)}${key}: ${stringify(value, depth + 1)}`);
+//     return `{\n${result.join('\n')}\n${generateSpaces(depth)}}`;
+//   }
+//   return node;
+// };
+
+// const stylish = (ast) => {
+//   const iter = (tree, depth) => {
+//     const space = generateSpaces(depth);
+//     const result = tree.map((node) => {
+//       switch (node.type) {
+//         case 'nested':
+//           return ` ${space}  ${node.key}: ${iter(node.children, depth + 1)}`;
+//         case 'unchanged':
+//           return ` ${space}  ${node.key}: ${stringify(node.value, depth + 1)}`;
+//         case 'added':
+//           return ` ${space}+ ${node.key}: ${stringify(node.value, depth + 1)}`;
+//         case 'removed':
+//           return ` ${space}- ${node.key}: ${stringify(node.value, depth + 1)}`;
+//         case 'changed': {
+//           const from = `${space}- ${node.name}: ${stringify(node.valueBefore, depth + 1)}`;
+//           const to = `${space}+ ${node.name}: ${stringify(node.valueAfter, depth + 1)}`;
+//           return `${from}\n${to}`;
+//         }
+//         default:
+//           throw new Error(`Unknown format type: ${node.type}`);
+//       }
+//     });
+//     // return `{\n${result.join('\n')}\n  ${generateSpaces(depth)}}`;
+//     return ['{', ...result, `${generateSpaces(depth)}}`].join('\n');
+//   };
+//   return iter(ast, 1);
+// };
+
+// export default stylish;
 import _ from 'lodash';
 
-const generateSpaces = (depth) => depth * 4 - 2;
+const generateSpacesWithSign = (multiplier) => ' '.repeat(4 * multiplier - 2);
 
-const stringify = (node, depth) => {
+const generateSpacesWithoutSign = (multiplier) => ' '.repeat(4 * multiplier);
+
+const checkObj = (node, multiplier) => {
   if (_.isObject(node)) {
     const entries = Object.entries(node);
-    const result = entries.map(([key, value]) => `${generateSpaces(depth + 1)}${key}: ${stringify(value, depth + 1)}`);
-    return `{\n${result.join('\n')}\n  ${generateSpaces(depth)}}`;
+    const deepObjects = entries
+      .reduce((acc, [key, value]) => `${acc}\n${generateSpacesWithoutSign(multiplier)}${key}: ${checkObj(value, multiplier + 1)}`, '');
+    return `{${deepObjects}\n${generateSpacesWithoutSign(multiplier - 1)}}`;
   }
   return node;
 };
 
 const stylish = (ast) => {
-  const iter = (tree, depth) => {
-    const space = generateSpaces(depth);
-    const result = tree.flatMap((node) => {
+  const iter = (tree, multiplier) => {
+    const spacesWithoutSign = generateSpacesWithoutSign(multiplier);
+    const spacesWithSign = generateSpacesWithSign(multiplier);
+    const result = tree.map((node) => {
       switch (node.type) {
-        case 'added':
-          return ` ${space}+ ${node.key}: ${stringify(node.value, depth + 1)}`;
-        case 'removed':
-          return ` ${space}- ${node.key}: ${stringify(node.value, depth + 1)}`;
-        case 'unchanged':
-          return ` ${space}  ${node.key}: ${stringify(node.value, depth + 1)}`;
         case 'nested':
-          return ` ${space}  ${node.key}: ${iter(node.children, depth + 1)}`;
-        case 'changed':
-          return [
-            `  ${space}- ${node.key}: ${stringify(node.value1, depth + 1)}`,
-            `  ${space}+ ${node.key}: ${stringify(node.value2, depth + 1)}`,
-          ];
+          return `${spacesWithoutSign}${node.name}: ${(iter(node.children, multiplier + 1))}`;
+        case 'unchanged':
+          return `${spacesWithoutSign}${node.name}: ${checkObj(node.value, multiplier + 1)}`;
+        case 'added':
+          return `${spacesWithSign}+ ${node.name}: ${checkObj(node.value, multiplier + 1)}`;
+        case 'removed':
+          return `${spacesWithSign}- ${node.name}: ${checkObj(node.value, multiplier + 1)}`;
+        case 'changed': {
+          const from = `${spacesWithSign}- ${node.name}: ${checkObj(node.valueBefore, multiplier + 1)}`;
+          const to = `${spacesWithSign}+ ${node.name}: ${checkObj(node.valueAfter, multiplier + 1)}`;
+          return `${from}\n${to}`;
+        }
         default:
-          throw new Error(`Unknown format type: ${node.type}`);
+          throw new Error(`Unexpected node type: ${node.type}`);
       }
     });
-    return `{\n${result.join('\n')}\n  ${generateSpaces(depth)}}`;
+    return ['{', ...result, `${generateSpacesWithoutSign(multiplier - 1)}}`].join('\n');
   };
   return iter(ast, 1);
 };
