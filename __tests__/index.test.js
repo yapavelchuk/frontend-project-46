67161b22fd7d8d52d3b9add88c88b5f243239c72
Yana Pavelchuk <yapavelchuk@yandex.ru>
2023-06-22 16:04:15 +0600

    index.test fixes

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,23 +1,23 @@
 import fs from 'fs';
 import path from 'path';
-import genDiff from '../src/index.js';
 import { expect } from 'expect';
+import genDiff from '../src/index.js';
 
 const fixturesPath = path.join(
   path.dirname(new URL(import.meta.url).pathname),
   '..',
-  '__fixtures__'
+  '__fixtures__',
 );
 
 const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
 const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
-const filepath = (filepath) => path.join(fixturesPath, filepath);
+const getFilepath = (filepath) => path.join(fixturesPath, filepath);
 const expectedPath = (expectedFile) => path.join(fixturesPath, expectedFile);
 
 test('get stylish comparison of JSON files', () => {
   const filepathExpected = expectedPath('stylishResult.txt');
   const expected = readFile(getAbsolutePath(filepathExpected));
-  const result = genDiff(filepath('file1.json'), filepath('file2.json'), 'stylish');
+  const result = genDiff(getFilepath('file1.json'), getFilepath('file2.json'), 'stylish');
 
   expect(result).toEqual(expected);
 });
@@ -25,7 +25,7 @@ test('get stylish comparison of JSON files', () => {
 test('get plain comparison of JSON files', () => {
   const filepathExpected = expectedPath('plainResult.txt');
   const expected = readFile(getAbsolutePath(filepathExpected));
-  const result = genDiff(filepath('file1.json'), filepath('file2.json'), 'plain');
+  const result = genDiff(getFilepath('file1.json'), getFilepath('file2.json'), 'plain');
 
   expect(result).toEqual(expected);
 });
@@ -33,7 +33,7 @@ test('get plain comparison of JSON files', () => {
 test('get json comparison of JSON files', () => {
   const filepathExpected = expectedPath('jsonResult.json');
   const expected = readFile(getAbsolutePath(filepathExpected));
-  const result = genDiff(filepath('file1.json'), filepath('file2.json'), 'json');
+  const result = genDiff(getFilepath('file1.json'), getFilepath('file2.json'), 'json');
 
   expect(result).toEqual(expected);
 });
@@ -41,7 +41,7 @@ test('get json comparison of JSON files', () => {
 test('get stylish comparison of YAML files', () => {
   const filepathExpected = expectedPath('stylishResult.txt');
   const expected = readFile(getAbsolutePath(filepathExpected));
-  const result = genDiff(filepath('file1.yml'), filepath('file2.yaml'), 'stylish');
+  const result = genDiff(getFilepath('file1.yml'), getFilepath('file2.yaml'), 'stylish');
 
   expect(result).toEqual(expected);
 });
@@ -49,7 +49,7 @@ test('get stylish comparison of YAML files', () => {
 test('get plain comparison of YAML files', () => {
   const filepathExpected = expectedPath('plainResult.txt');
   const expected = readFile(getAbsolutePath(filepathExpected));
-  const result = genDiff(filepath('file1.yml'), filepath('file2.yaml'), 'plain');
+  const result = genDiff(getFilepath('file1.yml'), getFilepath('file2.yaml'), 'plain');
 
   expect(result).toEqual(expected);
 });
@@ -57,7 +57,7 @@ test('get plain comparison of YAML files', () => {
 test('get json comparison of YAML files', () => {
   const filepathExpected = expectedPath('jsonResult.json');
   const expected = readFile(getAbsolutePath(filepathExpected));
-  const result = genDiff(filepath('file1.yml'), filepath('file2.yaml'), 'json');
+  const result = genDiff(getFilepath('file1.yml'), getFilepath('file2.yaml'), 'json');
 
   expect(result).toEqual(expected);
 });
