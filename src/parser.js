32eb36f08097a5b066feef5de67d6253ffebd450
Yana Pavelchuk <yapavelchuk@yandex.ru>
2023-06-26 16:50:39 +0600

    update parser

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,10 @@
 import yaml from 'js-yaml';
 
 const parseData = (fileData, fileExtension) => {
-  if (fileExtension === '.json') {
+  if (fileExtension === 'json') {
     return JSON.parse(fileData);
   }
-  if (fileExtension === '.yml' || fileExtension === '.yaml') {
+  if (fileExtension === 'yml' || fileExtension === 'yaml') {
     return yaml.load(fileData);
   }
   throw new Error(`Unsupported file format: ${fileExtension}`);
