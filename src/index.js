35333f1b3ca3710b5173ece088919e4f5cf3ec66
Yana Pavelchuk <yapavelchuk@yandex.ru>
2023-06-26 16:46:50 +0600

    update parser,js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+import fs from 'fs';
+import process from 'process';
+import path from 'path';
 import parseData from './parser.js';
 import buildTree from './buildTree.js';
 import formatter from './formatters/index.js';
 
+const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
+const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
+const getFileExtension = (filePath) => path.extname(filePath);
+
 const genDiff = (filepath1, filepath2, format) => {
-  const file1data = parseData(filepath1);
-  const file2data = parseData(filepath2);
-  const tree = buildTree(file1data, file2data);
+  const file1Ext = getFileExtension(filepath1).slice(1);
+  const file2Ext = getFileExtension(filepath2).slice(1);
+  const file1Data = readFile(getAbsolutePath(filepath1));
+  const file2Data = readFile(getAbsolutePath(filepath2));
+
+  const parsedData1 = parseData(file1Data, file1Ext);
+  const parsedData2 = parseData(file2Data, file2Ext);
+  const tree = buildTree(parsedData1, parsedData2);
   return formatter(tree, format);
 };
 
--- src/parser.js
+++ src/parser.js
@@ -1,16 +1,6 @@
 import yaml from 'js-yaml';
-import fs from 'fs';
-import process from 'process';
-import path from 'path';
 
-const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
-const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
-const getFileExtension = (filePath) => path.extname(filePath);
-
-const parseData = (filepath) => {
-  const fileExtension = getFileExtension(filepath);
-  const fileData = readFile(getAbsolutePath(filepath));
-
+const parseData = (fileData, fileExtension) => {
   if (fileExtension === '.json') {
     return JSON.parse(fileData);
   }
