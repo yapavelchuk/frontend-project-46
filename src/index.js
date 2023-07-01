104b55ae838327a13a07f7237f7e28624a00f78b
Yana Pavelchuk <yapavelchuk@yandex.ru>
2023-07-01 12:07:20 +0600

    index.js changes

--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ import buildTree from './buildTree.js';
 import formatter from './formatters/index.js';
 
 const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
-const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
-const getFileExtension = (filePath) => path.extname(filePath);
+
+const getData = (filePath) => {
+  const fileExt = path.extname(filePath).slice(1);
+  const fileData = fs.readFileSync(getAbsolutePath(filePath), 'utf-8');
+  const parsedData = parseData(fileData, fileExt);
+  return parsedData;
+};
 
 const genDiff = (filepath1, filepath2, format) => {
-  const file1Ext = getFileExtension(filepath1).slice(1);
-  const file2Ext = getFileExtension(filepath2).slice(1);
-  const file1Data = readFile(getAbsolutePath(filepath1));
-  const file2Data = readFile(getAbsolutePath(filepath2));
-
-  const parsedData1 = parseData(file1Data, file1Ext);
-  const parsedData2 = parseData(file2Data, file2Ext);
+  const parsedData1 = getData(filepath1);
+  const parsedData2 = getData(filepath2);
   const tree = buildTree(parsedData1, parsedData2);
   return formatter(tree, format);
 };
